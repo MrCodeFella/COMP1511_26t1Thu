524955e9198b28f96ffbb05148214d4761647dd2
conrad <z5478184@ad.unsw.edu.au>
2026-02-23 13:30:28 +1100

    wk 1

---
+++ 01/starter_code/face.c
@@ -0,0 +1,29 @@
+// face.c
+//
+// Written by Conrad(z5478184),
+// on 19/02
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+#include <stdio.h>
+
+int main(void) {
+    printf(" ~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+
+    return 0;
+}
